3f27e421e2ea5e016ef997174d0b008fc9dbbd83
dibi babu <dibikannan@gmail.com>
2022-12-01 15:17:33 +0530

    added axios .create

--- src/api/UserRequest.js
+++ src/api/UserRequest.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.({baseURL: "https://meeter.gq"})
+const API = axios.create({baseURL: "https://meeter.gq"})
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
